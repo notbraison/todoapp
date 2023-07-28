7eacaa80695201554da007941ce11465a42650ba
notbraison <orinabraison@gmail.com>
2023-07-28 09:46:32 +0300

    i guess its working

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/screens/forgot_password.dart'
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  runApp(const ToDoApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class ToDoApp extends StatelessWidget {
+  const ToDoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'To Do App',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginScreen(),
+        '/Home': (context) => const HomeScreen(),
+        '/Register': (context) => const RegisterScreen(),
+        '/ForgotPassword': (context) => const ForgotPassword(),
+      },
+
+      theme: ThemeData(
+          colorScheme:
+              ColorScheme.fromSeed(seedColor: const Color(0xFF00FF00))
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+              ),
     );
   }
 }
